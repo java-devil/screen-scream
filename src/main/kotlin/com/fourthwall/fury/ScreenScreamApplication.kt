37b895756ba5d5cfe54361fc8f8df4b82ecb864e
Adam Dymek <adam@dymek.dev>
2024-12-07 11:45:00 +0100

    init: universe

---
+++ src/main/kotlin/com/fourthwall/fury/ScreenScreamApplication.kt
@@ -0,0 +1,11 @@
+package com.fourthwall.fury
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScreenScreamCinemaApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScreenScreamCinemaApplication>(*args)
+}
---
+++ src/test/kotlin/com/fourthwall/fury/ScreenScreamApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.fourthwall.fury
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScreenScreamApplicationTests {
+
+	@Test
+	fun contextLoads() {}
+}
