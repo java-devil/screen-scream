0bbdf203493d4ecbc45adb86e1e1467d7089db5f
Adam Dymek <adam@dymek.dev>
2024-12-09 20:00:00 +0100

    chore: true persistence capabilities

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jooq.meta.jaxb.Logging
+import org.jooq.meta.jaxb.Property
+
 plugins {
 	kotlin("jvm") version "1.9.25"
 	kotlin("plugin.spring") version "1.9.25"
+	id("nu.studer.jooq") version "9.0"
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
 }
@@ -22,10 +26,19 @@ dependencies {
 	implementation("io.arrow-kt:arrow-core:1.2.4")
 	implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-jooq")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.postgresql:postgresql:42.7.4")
+	jooqGenerator("org.postgresql:postgresql:42.7.4")
+	implementation("org.flywaydb:flyway-core")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("io.rest-assured:rest-assured")
+	testImplementation("io.rest-assured:json-path")
+	testImplementation("org.testcontainers:postgresql")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
@@ -37,4 +50,47 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	version.set("3.19.15")
+	edition.set(nu.studer.gradle.jooq.JooqEdition.OSS)
+
+	configurations {
+		create("main") {
+			generateSchemaSourceOnCompilation.set(false)
+
+			jooqConfiguration.apply {
+				logging = Logging.WARN
+				jdbc.apply {
+					driver = "org.postgresql.Driver"
+					url = "jdbc:postgresql://db:5432/screen-scream"
+					user = "thisisnotaproductionusername"
+					password = "thisisnotaproductionpassword"
+					properties.add(Property().apply {
+						key = "ssl"
+						value = "false"
+					})
+				}
+				generator.apply {
+					name = "org.jooq.codegen.DefaultGenerator"
+					database.apply {
+						name = "org.jooq.meta.postgres.PostgresDatabase"
+						inputSchema = "public"
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = true
+						isImmutablePojos = true
+						isFluentSetters = true
+					}
+					target.apply {
+						packageName = "nu.studer.sample"
+						directory = "build/jooq/main"
+					}
+					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+				}
+			}
+		}
+	}
 }
